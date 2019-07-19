8536fe5f7ae403145dacbf90acf82782a1814eb6
Carlos <carlosmoro95@gmail.com>
2019-07-19 11:49:26 +0200

    Select a project

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,6 +1,13 @@
 export type User = {
   admin: boolean,
-  allowLanguages: number[]
+  allowLanguages: number[],
+  allowProjects: number[]
+};
+
+export type Project = {
+  id: number,
+  name: string,
+  languages: number[]
 };
 
 export type Language = {
@@ -13,11 +20,13 @@ export type Translation = {
   id: number,
   lang_id: number,
   lit_id: number,
+  project_id: number,
   translation: string
 };
 
 export type Literal = {
   id: number,
+  project_id: number,
   literal: string,
   as_in: string
 }
