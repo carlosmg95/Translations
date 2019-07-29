d204946505e5e11f2b36d0097f009169e7bdb6f3
Carlos <carlosmoro95@gmail.com>
2019-07-29 08:48:33 +0200

    Add mutation createProject

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,23 +2,25 @@ import { GraphQLServer } from 'graphql-yoga';
 import { typeDefs } from '../prisma/generated/prisma-client/prisma-schema';
 import { Prisma } from 'prisma-binding';
 import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
 
 const prisma = new Prisma({
   typeDefs,
-  endpoint: 'http://localhost:4466'
+  endpoint: 'http://localhost:4466',
 });
 
 const server: GraphQLServer = new GraphQLServer({
   typeDefs,
   resolvers: {
-    Query
+    Query,
+    Mutation,
   },
   context: {
-    prisma
+    prisma,
   },
   resolverValidationOptions: {
-    requireResolversForResolveType: false
-  }
+    requireResolversForResolveType: false,
+  },
 });
 
 server.start(() => {
