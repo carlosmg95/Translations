4bd7969871d7c22b356c386186d84773a0a7e57a
Carlos <carlosmoro95@gmail.com>
2019-08-06 15:37:56 +0200

    Throw error when translation can't connect

--- api/src/resolvers/Mutation.ts
+++ api/src/resolvers/Mutation.ts
@@ -1,3 +1,9 @@
+const throwError = (message?: string): void => {
+  const errorMessage = message || 'Error ocurred.';
+  console.error(`[error] ${errorMessage}`);
+  throw new Error(errorMessage);
+};
+
 const Mutation = {
   async createProject(parent, { data }, { prisma }, info) {
     console.info('[info] Mutation: createProject');
@@ -13,11 +19,7 @@ const Mutation = {
       project: { name: projectName },
     });
 
-    if (existsLiteral) {
-      const errorMessage: string = 'The literal already exists.';
-      console.error(`[error] ${errorMessage}`);
-      throw new Error(errorMessage);
-    }
+    if (existsLiteral) throwError('The literal already exists.');
 
     return await prisma.mutation.createTranslation({ data }, info);
   },
@@ -25,6 +27,25 @@ const Mutation = {
     console.info('[info] Mutation: upsertTranslation');
 
     const translationExists: boolean = await prisma.exists.Translation(where);
+    if (!translationExists) {
+      const languageId: string = create.language.connect.id;
+      const literalId: string = create.literal.connect.id;
+      const projectName: string = create.project.connect.name;
+
+      const languageExists: boolean = await prisma.exists.Language({
+        id: languageId,
+      });
+      const literalExists: boolean = await prisma.exists.Literal({
+        id: literalId,
+        project: { name: projectName },
+      });
+      const projectExists: boolean = await prisma.exists.Project({
+        name: projectName,
+      });
+
+      if (!languageExists || !literalExists || !projectExists)
+        throwError("It can't connect with the requiered elements.");
+    }
     return await prisma.mutation.upsertTranslation(
       { where, create, update },
       info,
