94e624f0cf46786f5b939d0b1032a8cd4323b7e6
Carlos <carlosmoro95@gmail.com>
2019-08-09 10:53:47 +0200

    Set graphql schema inmutable

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,16 @@
 import { GraphQLServer } from 'graphql-yoga';
-import { typeDefs } from '../prisma/generated/prisma-client/prisma-schema';
+//import { typeDefs } from './schema.graphql';
 import { Prisma } from 'prisma-binding';
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
 
 const prisma = new Prisma({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   endpoint: 'http://localhost:4466',
 });
 
 const server: GraphQLServer = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
     Mutation,
