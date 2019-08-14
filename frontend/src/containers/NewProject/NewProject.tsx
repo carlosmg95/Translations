04f234483576410aa2a0aa75b574cc9b0f292352
Carlos <carlosmoro95@gmail.com>
2019-08-14 13:39:46 +0200

    Improve create new project code

--- frontend/src/containers/NewProject/NewProject.tsx
+++ frontend/src/containers/NewProject/NewProject.tsx
@@ -17,76 +17,147 @@ interface NewProjectProps {
 
 const NewProject: React.FC<NewProjectProps> = (props: NewProjectProps) => {
   const [nameState, setName]: [
+    // Project name
     string,
     Dispatch<SetStateAction<string>>,
   ] = useState('');
 
   const [usersState, setUsersState]: [
+    // List of allowed users
     Set<string>,
     Dispatch<SetStateAction<Set<string>>>,
   ] = useState(new Set());
 
   const [languagesState, setLanguagesState]: [
+    // List of languages in the project
     Set<string>,
     Dispatch<SetStateAction<Set<string>>>,
   ] = useState(new Set());
 
   const [languageRegex, setLanguageRegex]: [
+    // String with a part of the name of a language
     string,
     Dispatch<SetStateAction<string>>,
   ] = useState('');
 
   const [userRegex, setUserRegex]: [
+    // String with a part of the name of an user
     string,
     Dispatch<SetStateAction<string>>,
   ] = useState('');
 
   const [errorMessageState, setErrorMessageState] = useState({
+    // Errors messages
     name: '',
     languages: '',
     users: '',
   });
 
+  // Actions when the form changes
   const changeForm = (event: any) => {
     let errorMessage = { ...errorMessageState };
 
     if (event.target.className.match(/.*project-name.*/)) {
+      // If the name changes
       const name: string = event.target.value;
+
       if (!name || name.match(/\s|\.|\//gi))
         errorMessage.name = "The name is empty or it's wrong";
       else errorMessage.name = '';
+
       setName(name);
     } else if (event.target.className.match(/.*project-users__checkbox.*/)) {
+      // If an user is selected
       let users: Set<string> = new Set(usersState);
+
       if (event.target.checked) {
         users.add(event.target.value);
       } else {
         users.delete(event.target.value);
       }
+
       if (users.size === 0)
         errorMessage.users = 'You must select one user at least';
       else errorMessage.users = '';
+
       setUsersState(users);
     } else if (
+      // If a language is selected
       event.target.className.match(/.*project-languages__checkbox.*/)
     ) {
       let languages: Set<string> = new Set(languagesState);
+
       if (event.target.checked) {
         languages.add(event.target.value);
       } else {
         languages.delete(event.target.value);
       }
+
       if (languages.size === 0)
         errorMessage.languages = 'You must select one language at least';
       else errorMessage.languages = '';
+
       setLanguagesState(languages);
     } else if (event.target.className.match(/.*language-search.*/)) {
+      // If a language is being searched
       setLanguageRegex(event.target.value);
     } else if (event.target.className.match(/.*user-search.*/)) {
+      // If an user is being searched
       setUserRegex(event.target.value);
     }
 
     setErrorMessageState(errorMessage);
+  };
+
+  const createNewProject = (mutation): void => {
+    let errorMessage = { ...errorMessageState };
+
+    if (!nameState || nameState.match(/\s|\.|\//gi))
+      errorMessage.name = "The name is empty or it's wrong";
+
+    if (usersState.size === 0)
+      errorMessage.users = 'You must select one user at least';
+
+    if (languagesState.size === 0)
+      errorMessage.languages = 'You must select one language at least';
+
+    setErrorMessageState(errorMessage);
+
+    if (errorMessage.languages || errorMessage.users || errorMessage.name)
+      return;
+
+    mutation({
+      variables: {
+        project: {
+          name: nameState,
+          users: {
+            connect: Array.from(usersState).map((userId: string) => {
+              return { id: userId };
+            }),
+          },
+          languages: {
+            connect: Array.from(languagesState).map((languageId: string) => {
+              return { id: languageId };
+            }),
+          },
+        },
+      },
+    })
+      .then(result => {
+        const project: Project = result.data.createProject;
+
+        project.translations = [];
+        props.addNewProject(project);
+      })
+      .catch(e => {
+        const field = e.message.replace(/.*\s(\w+)$/, '$1');
+        const errorMessage = { ...errorMessageState };
+
+        if (field === 'name') {
+          errorMessage.name = 'The name cannot be repeated';
+          setErrorMessageState(errorMessage);
+        }
+      });
   };
 
   const USERS_LANGUAGES = gql`
@@ -125,6 +196,7 @@ const NewProject: React.FC<NewProjectProps> = (props: NewProjectProps) => {
   `;
 
   if (!props.user.admin) {
+    // Only admins can create a new project
     return <ErrorMessage code={403} message="You shouldn't be here!" />;
   }
 
@@ -135,73 +207,23 @@ const NewProject: React.FC<NewProjectProps> = (props: NewProjectProps) => {
         links={[{ to: '/dashboard', text: 'dashboard' }]}
       />
       <DashboardBody>
+        {/* CANCEL BUTTON */}
         <Link to="/dashboard" className="btn btn-cancel">
           Cancel
         </Link>
+        {/* SAVE BUTTON */}
         <Mutation mutation={CREATE_PROJECT}>
           {createProject => (
             <Link
               to="/dashboard"
               className="btn btn-save"
-              onClick={() => {
-                let errorMessage = { ...errorMessageState };
-
-                if (!nameState || nameState.match(/\s|\.|\//gi))
-                  errorMessage.name = "The name is empty or it's wrong";
-                if (usersState.size === 0)
-                  errorMessage.users = 'You must select one user at least';
-                if (languagesState.size === 0)
-                  errorMessage.languages =
-                    'You must select one language at least';
-
-                setErrorMessageState(errorMessage);
-                if (
-                  errorMessage.languages ||
-                  errorMessage.users ||
-                  errorMessage.name
-                )
-                  return;
-
-                createProject({
-                  variables: {
-                    project: {
-                      name: nameState,
-                      users: {
-                        connect: Array.from(usersState).map(
-                          (userId: string) => {
-                            return { id: userId };
-                          },
-                        ),
-                      },
-                      languages: {
-                        connect: Array.from(languagesState).map(
-                          (languageId: string) => {
-                            return { id: languageId };
-                          },
-                        ),
-                      },
-                    },
-                  },
-                })
-                  .then(result => {
-                    const project: Project = result.data.createProject;
-                    project.translations = [];
-                    props.addNewProject(project);
-                  })
-                  .catch(e => {
-                    const field = e.message.replace(/.*\s(\w+)$/, '$1');
-                    const errorMessage = { ...errorMessageState };
-                    if (field === 'name') {
-                      errorMessage.name = 'The name cannot be repeated';
-                      setErrorMessageState(errorMessage);
-                    }
-                  });
-              }}
+              onClick={() => createNewProject(createProject)}
             >
               Save
             </Link>
           )}
         </Mutation>
+        {/* FORM */}
         <Query query={USERS_LANGUAGES}>
           {({ data, loading }) => {
             if (loading) {
@@ -210,6 +232,7 @@ const NewProject: React.FC<NewProjectProps> = (props: NewProjectProps) => {
             const { users, languages } = data;
             return (
               <form onChange={changeForm} className="new-project__form">
+                {/* FORM NAME */}
                 <div className="form-group">
                   <label className="form-item">Name: </label>
                   <input className="form-item project-name" type="text" />
@@ -221,7 +244,9 @@ const NewProject: React.FC<NewProjectProps> = (props: NewProjectProps) => {
                     ''
                   )}
                 </div>
+                {/* FORM LANGUAGES */}
                 <div className="form-group search-group">
+                  {/* SEARCH INPUT */}
                   <div className="input-search">
                     <input
                       className="form-item search language-search"
@@ -236,6 +261,7 @@ const NewProject: React.FC<NewProjectProps> = (props: NewProjectProps) => {
                   ) : (
                     ''
                   )}
+                  {/* SEARCH RESULTS */}
                   <div className="result-search">
                     {languages
                       .filter(
@@ -260,7 +286,9 @@ const NewProject: React.FC<NewProjectProps> = (props: NewProjectProps) => {
                       ))}
                   </div>
                 </div>
+                {/* FORM USERS */}
                 <div className="form-group search-group">
+                  {/* SEARCH INPUT */}
                   <div className="input-search">
                     <input
                       className="form-item search user-search"
@@ -275,6 +303,7 @@ const NewProject: React.FC<NewProjectProps> = (props: NewProjectProps) => {
                   ) : (
                     ''
                   )}
+                  {/* SEARCH RESULTS */}
                   <div className="result-search">
                     {users
                       .filter(
