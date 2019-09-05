d429ae6b1e89d9fb52c3a62aff7f0857d17939fa
Carlos <carlosmoro95@gmail.com>
2019-09-05 07:59:23 +0200

    Creates a modal component

---
+++ frontend/src/components/Modal/Modal.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Modal.css';
+import PillButton from '../PillButton/PillButton';
+
+interface modalProps {
+  title: string;
+  children: JSX.Element | JSX.Element[] | string;
+  acceptFunction(): void;
+  cancelFunction(): void;
+}
+
+const modal: React.FC<modalProps> = (props: modalProps) => {
+  return (
+    <div className="Modal">
+      <div className="modal-content">
+        <h1>{props.title}</h1>
+        <div className="modal-body">{props.children}</div>
+        <div className="modal-buttons">
+          <PillButton
+            className="cancel"
+            text="Cancel"
+            onClick={props.cancelFunction}
+          />
+          <PillButton text="Upload" onClick={props.acceptFunction} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default modal;
