0cc58adc24bce4bbeb597e6c966d68fa2904d0f2
Carlos <carlosmoro95@gmail.com>
2019-09-05 09:17:31 +0200

    Changes style

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,57 +1,57 @@
 export type User = {
-  id: string
-  name: string
-  admin: Boolean
-  projects: Project[]
-  languages: Language[]
-}
+  id: string;
+  name: string;
+  admin: Boolean;
+  projects: Project[];
+  languages: Language[];
+};
 
 export type Project = {
-  id: string
-  name: string
-  git_repo: string
-  git_name: string
-  git_branch: string
-  git_path: string
-  users: User[]
-  languages: Language[]
-  translations: Translation[]
-  literals: Literal[]
-}
+  id: string;
+  name: string;
+  git_repo: string;
+  git_name: string;
+  git_branch: string;
+  git_path: string;
+  users: User[];
+  languages: Language[];
+  translations: Translation[];
+  literals: Literal[];
+};
 
 export type Language = {
-  id: string
-  name: string
-  iso: string
-  code: string
-  projects: Project[]
-  users: User[]
-  translations: Translation[]
-}
+  id: string;
+  name: string;
+  iso: string;
+  code: string;
+  projects: Project[];
+  users: User[];
+  translations: Translation[];
+};
 
 export type Translation = {
-  id: string
-  language: Language
-  literal: Literal
-  project: Project
-  translation: string
-}
+  id: string;
+  language: Language;
+  literal: Literal;
+  project: Project;
+  translation: string;
+};
 
 export type Literal = {
-  id: string
-  project: Project
-  translations: Translation[]
-  literal: string
-  as_in: string
-}
+  id: string;
+  project: Project;
+  translations: Translation[];
+  literal: string;
+  as_in: string;
+};
 
 export type LiteralTranslation = {
-  literalId: string
-  translationId: string
-  literal: string
-  as_in: string
-  translation: string
-}
+  literalId: string;
+  translationId: string;
+  literal: string;
+  as_in: string;
+  translation: string;
+};
 
 export enum Filter {
   ALL,
